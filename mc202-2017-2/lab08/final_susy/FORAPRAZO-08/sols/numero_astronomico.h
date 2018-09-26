40a2827b0223a89e014cb630860d181c5f7dbda9
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2018-09-26 12:00:00 -0300

    final version of lab08

---
+++ mc202-2017-2/lab08/final_susy/FORAPRAZO-08/sols/numero_astronomico.h
@@ -0,0 +1,44 @@
+#ifndef ASTRO_NUMBER_H
+#define ASTRO_NUMBER_H
+
+
+#define MAX_NUMBER_SIZE_IN 1000
+
+
+typedef struct node {
+    int number;
+    struct node * next;
+    struct node * prev;
+} Node;
+typedef Node * p_node;
+
+p_node create_node(int number, p_node next, p_node prev);
+
+void delete_node(p_node node);
+
+typedef struct {
+    int sign;
+    int digits;
+    p_node head;
+    p_node tail;
+} AstroNumber;
+
+typedef AstroNumber * p_astro_number;
+
+p_astro_number create_astro_number(char *number);
+
+void delete_astro_number(p_astro_number astro_number);
+
+p_astro_number inverse(p_astro_number a);
+p_astro_number sum(p_astro_number a, p_astro_number b);
+p_astro_number subtract(p_astro_number a, p_astro_number b);
+p_astro_number multiply(p_astro_number a, p_astro_number b);
+p_astro_number multiply_by_digit(p_astro_number a, int digit);
+p_astro_number add_zeros_in_tail(p_astro_number a, int n_zeros);
+p_astro_number division(p_astro_number a, p_astro_number b);
+
+int cmp(p_astro_number a, p_astro_number b);
+void print_astro_number(p_astro_number astro_number);
+void delete_left_zeros(p_astro_number a);
+
+#endif
---
+++ mc202-2017-2/lab08/final_susy/SANDBOX-08/sols/lab08.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "numero_astronomico.h"
+
+#define MAX_SIZE_OPERATION 4
+
+int main(int argc, char const *argv[]) {
+    char NUM_IN[MAX_NUMBER_SIZE_IN], op[MAX_SIZE_OPERATION];
+    p_astro_number a, b, c;
+
+    a = create_astro_number("0");
+
+    while(1) {
+        scanf("%s", op);
+
+        if (op[0] == '#') {
+            break;
+        } else if (op[0] == '+') {
+            scanf("%s", NUM_IN);
+            b = create_astro_number(NUM_IN);
+            c = sum(a, b);
+            delete_astro_number(a);
+            delete_astro_number(b);
+        } else if (op[0] == '-') {
+            scanf("%s", NUM_IN);
+            b = create_astro_number(NUM_IN);
+            c = subtract(a, b);
+            delete_astro_number(a);
+            delete_astro_number(b);
+        }  else if (op[0] == '*') {
+            scanf("%s", NUM_IN);
+            b = create_astro_number(NUM_IN);
+            c = multiply(a, b);
+            delete_astro_number(a);
+            delete_astro_number(b);
+        } else if (op[0] == 'i') {
+            c = inverse(a);
+            delete_astro_number(a);
+        }
+        a = c;
+        print_astro_number(a);
+    }
+
+    delete_astro_number(a);
+    return 0;
+}
